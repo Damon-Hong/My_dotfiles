8f2da2ef3f9ce82bb2296a00b4860fb1e90782ed
Damon Hong <damon.hong.q.y@gmail.com>
2020-08-07 00:15:00 +0800

    first commit

---
+++ aliases.sh
@@ -0,0 +1,80 @@
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
+# Google Chrome
+# alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
+
+# Automatically use GNU ls
+alias "ls=ls -N --color=auto"
+
+## Applications shortcuts ##
+
+# Google Chrome
+alias Chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+
+# Firefox
+alias Firefox="/Applications/Firefox.app/Contents/MacOS/firefox-bin"
+
+# Productivity
+alias Evernote="open -a /Applications/Evernote.app/"
+alias PDFExpert="open -a /Applications/PDF\ Expert.app/"
+alias MindNode="open -a /Applications/MindNode.app/"
+alias AdobeReader="open -a /Applications/Adobe\ Acrobat\ Reader\ DC.app/"
+alias Kindle="open -a /Applications/Kindle.app/"
+
+# Microsoft apps
+alias Word="open -a /Applications/Microsoft\ Word.app/"
+alias Excel="open -a /Applications/Microsoft\ Excel.app/"
+alias PowerPoint="open -a /Applications/Microsoft\ PowerPoint.app/"
+alias OneNote="open -a /Applications/Microsoft\ OneNote.app/"
+alias Outlook="open -a /Applications/Microsoft\ Outlook.app/"
+alias OneDrive="open -a /Applications/OneDrive.app/"
+alias Teams="open -a /Applications/Microsoft\ Teams.app/"
+
+# Apple apps
+alias Pages="open -a /Applications/Pages.app/"
+alias Numbers="open -a /Applications/Numbers.app/"
+alias Keynote="open -a /Applications/Keynote.app/"
+alias iMovie="open -a /Applications/iMovie.app/"
+
+# Social apps
+alias KakaoTalk="open -a /Applications/KakaoTalk.app/"
+alias Messenger="open -a /Applications/Messenger.app/"
+alias Skype="open -a /Applications/Skype.app/"
+alias Twitter="open -a /Applications/Twitter.app/"
+alias WeChat="open -a /Applications/WeChat.app/"
+alias WhatsApp="open -a /Applications/WhatsApp.app/"
+alias Zoom="open -a /Applications/zoom.us.app/"
+
+# Game
+alias Township="open -a /Applications/Township.app/"
+alias Steam="open -a /Applications/Steam.app/"
+alias LoL="open -a /Applications/League\ of\ Legends.app/"
+
+# VPNs
+alias ExpressVPN="open -a /Applications/ExpressVPN.app/"
+alias HotspotShield="open -a /Applications/Hotspot\ Shield.app/"
+alias NordVPN="open -a /Applications/NordVPN\ IKE.app/"
+
+# Editors
+#alias VSCode="open -a /Applications/Visual\ Studio\ Code.app/"
+alias VS="open -a /Applications/Visual\ Studio.app/"
+alias Xcode="open -a /Applications/Xcode.app/"
+alias RStudio="open -a /Applications/RStudio.app/"
+alias PyCharm="open -a /Applications/PyCharm\ CE.app/"
+alias MATLAB="open -a /Applications/MATLAB_R2019b.app/"
+# alias Jupyter="open -a /Applications/Anaconda-Navigator.app/"
+
+# Music
+alias Spotify="open -a /Applications/Spotify.app/"
+
+
